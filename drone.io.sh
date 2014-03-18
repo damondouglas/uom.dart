4081d485b83e86fa7e15788c97af721529c27302
Damon Douglas <douglas.damon@gmail.com>
2014-03-18 17:34:07 -0400

    drone.io.sh script for drone.io

---
+++ drone.io.sh
@@ -0,0 +1,7 @@
+dart tool/hop_runner.dart analyze_lib 
+dart test/weight_test.dart
+dart test/uom_test.dart
+dart test/volume_test.dart    
+dart test/height_test.dart
+dart test/ratio_test.dart
+  
